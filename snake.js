799850cdf9f9ae84986337432bc7cba786002962
Nickthegoatboy <nickthegoatboy@yahoo.com>
2018-10-04 16:57:26 -0600

    Added coordinate class and food object

--- snake.js
+++ snake.js
@@ -1,7 +1,24 @@
+
+
 class Snake{
   constructor(parts, dir){
   	this.parts = parts;
   	this.dir = dir;
+  }
+}
+
+class Food{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    }
+
+  move(snake) {
+    this.x = Math.floor(Math.random(width) + 1);
+    this.y = Math.floor(Math.random(height) + 1);
+    for(var part in snake.parts) {
+      console.log(part[0]);
+    }
   }
 }
 
@@ -13,8 +30,11 @@ let gradient = context.createLinearGradient(0, 0, canvas.width, canvas.height);
 gradient.addColorStop(0, "#DAE1ED");
 gradient.addColorStop(1, "#535456");
 
-canvas.width = 25 * res;
-canvas.height = 25 * res;
+let width = 25;
+let height = 25;
+
+canvas.width = width * res;
+canvas.height = height * res;
 
 
 window.addEventListener("keydown", keyListener, false);
@@ -37,44 +57,53 @@ function drawSnake(){
 	context.fillStyle = gradient;
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	context.fillStyle = "#000000";
-	context.fillRect(s.parts[0][0], s.parts[0][1], res, res);
+	context.fillRect(s.parts[0].x, s.parts[0].y, res, res);
+}
+
+function drawFood(){
+	context.fillStyle = "#FF0000";
+	context.fillRect(f.x * res, f.y * res, res, res);
 }
 
 function updateSnake(){
 	if(s.dir == "up"){
-		s.parts[0][1] -= res;
-		if(s.parts[0][1] < 0){
-			s.parts[0][1] = canvas.height - res;
+		s.parts[0].y -= res;
+		if(s.parts[0].y < 0){
+			s.parts[0].y = canvas.height - res;
 		}
 	}
 	else if(s.dir == "right"){
-    s.parts[0][0] += res;
-    if(s.parts[0][0] + res > canvas.width){
-      s.parts[0][0] = 0;
+    s.parts[0].x += res;
+    if(s.parts[0].x + res > canvas.width){
+      s.parts[0].x = 0;
     }
 	}
 	else if(s.dir == "down"){
-    s.parts[0][1] += res;
-    if(s.parts[0][1] + res > canvas.height){
-      s.parts[0][1] = 0;
+    s.parts[0].y += res;
+    if(s.parts[0].y + res > canvas.height){
+      s.parts[0].y = 0;
     }
 	}
 	else if(s.dir == "left"){
-    s.parts[0][0] -= res;
-    if(s.parts[0][0] < 0){
-      s.parts[0][0] = canvas.width - res;
+    s.parts[0].x -= res;
+    if(s.parts[0].x < 0){
+      s.parts[0].x = canvas.width - res;
     }
 	}
 }
+let snakeLocation = new Coordinate(0, 0);
+let s = new Snake([snakeLocation], "right");
+let f = new Food(Math.floor(width / 2), Math.floor(height / 2));
 
-let s = new Snake([[0, 0]], "right");
 console.log(s);
 
 drawSnake();
+drawFood();
 console.log(canvas);
 
 function draw(){
   frameRate(10);
   updateSnake();
   drawSnake();
+  drawFood();
 }
