8f69b42a94a76317f8d662baadcceb8273ce930a
David Ayres <dlayres@bb136-08.mines.edu>
2018-10-12 16:03:27 -0600

    Added separate p5 library

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 let size = 50; // Number of snake pixels the canvas size is
 let res = 15; // Number of pixels per "snake pixel"
+let startButton;
 let width = size * res; // Canvas width and height
 let height = size * res;
 let snakeLocation = new Coordinate(0, 0);
+let gameStarted = false;
 let s = new Snake([snakeLocation], "right", res);
 let f = new Food(Math.floor(width / res / 2), Math.floor(height / res / 2));
 
@@ -11,6 +13,7 @@ function setup(){
 	canvas.parent('canvas-holder');
   frameRate(10);
 	update_scores(); // update scores for high score API
+
 }
 
 function keyPressed(){
@@ -41,17 +44,30 @@ function keyPressed(){
 
 
 function draw(){
-  clear();
-  background(180);
-  s.checkSelfIntersect();
-  s.updateSnake();
-  s.checkBounds(width, height);
-  s.checkEat(f, res);
-  s.drawSnake();
-  f.drawFood(res);
+	if(gameStarted){
+  	clear();
+  	background(180);
+  	s.checkSelfIntersect();
+  	s.updateSnake();
+  	s.checkBounds(width, height);
+  	s.checkEat(f, res);
+  	s.drawSnake();
+  	f.drawFood(res);
+	}
+	else{
+		background(166, 145, 247);
+
+		fill(color(110, 86, 201));
+		rectMode(CENTER);
+		rect(width / 2, height / 2, 100, 100);
+	}
 }
 
 
 function updateHighScore(score){
 	highscore(score);  // submit to API
+}
+
+function startGame(){
+	gameStarted = !gameStarted;
 }
