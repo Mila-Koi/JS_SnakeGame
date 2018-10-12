1e50ea8c3b0c1e4a7c60a382445253eefaa67d81
David Ayres <dlayres@bb136-08.mines.edu>
2018-10-12 16:34:08 -0600

    Added starting screen

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
 let size = 50; // Number of snake pixels the canvas size is
 let res = 15; // Number of pixels per "snake pixel"
-let startButton;
+let gameLogo;
 let width = size * res; // Canvas width and height
-let height = size * res;
+let height = (size - 10) * res;
 let snakeLocation = new Coordinate(0, 0);
 let gameStarted = false;
 let s = new Snake([snakeLocation], "right", res);
 let f = new Food(Math.floor(width / res / 2), Math.floor(height / res / 2));
+
+function preload(){
+	gameLogo = loadImage("./thumbnail.png");
+}
 
 function setup(){
   var canvas = createCanvas(width, height);
@@ -14,6 +18,10 @@ function setup(){
   frameRate(10);
 	update_scores(); // update scores for high score API
 
+}
+
+function mouseOverButton(){
+	return ((mouseX > (width / 2 - 110)) && (mouseX < (width / 2 + 110)) && (mouseY > (height / 2 + 125)) && (mouseY < (height / 2 + 195)));
 }
 
 function keyPressed(){
@@ -57,9 +65,22 @@ function draw(){
 	else{
 		background(166, 145, 247);
 
-		fill(color(110, 86, 201));
+		if(mouseOverButton()){
+			fill(color(204, 170, 255));
+		}
+		else{
+			fill(color(110, 86, 201));
+		}
 		rectMode(CENTER);
-		rect(width / 2, height / 2, 100, 100);
+		rect(width / 2, height / 2 + 160, 220, 70);
+		imageMode(CENTER);
+		image(gameLogo, width/2, height / 3.5 + 50, gameLogo.width / 1.5, gameLogo.height / 1.5);
+		textAlign(CENTER);
+		textSize(30);
+		fill(color(255, 255, 255));
+		text("Start Game", width/2, height - 128);
+
+
 	}
 }
 
