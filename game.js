9e18126423646ba728fa424fef68a607f5343981
Tanner Jones <tfjones@bb136-09.mines.edu>
2018-10-12 17:40:05 -0600

    fixed stopping of timer

--- game.js
+++ game.js
@@ -116,12 +116,12 @@ function updateHighScore(score){
 
 
 
-
+var timeBoi;
 function startGame(){
 	gameStarted = !gameStarted;
   var sec = 0;
   function pad ( val ) { return val > 9 ? val : "0" + val; }
-  var timeBoi = setInterval( function(){
+  timeBoi = setInterval( function(){
       document.getElementById("seconds").innerHTML=pad(++sec%60);
       document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
   }, 1000);
